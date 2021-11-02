611ec46d3aeaec618f698efc92612c6492c52ed2
Kyler <kyler.b@live.com>
2021-11-02 13:21:05 -0500

    commit tree ee4dd450a35761fb9c0b82836ce9c7886d88dde3

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+    if(distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    }else if(distanceTravelledInFeet(start, destination) >= 2500) {
+        return 'cannot travel that far';
+    }else if(distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    }else {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    }
+}
